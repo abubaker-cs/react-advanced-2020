69ccfe8ecb1ec9aa8966196e9557774bb97183c6
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-02-02 20:13:01 +0500

    Studying about useState() Hook

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React from "react";
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  let title = "Welcome to React";
+
+  const handleClick = () => {
+    title = "Goodbye";
+    console.log(title);
+  };
+
+  return (
+    <React.Fragment>
+      <h2>{title}</h2>
+      <button type="button" className="btn" onClick={handleClick}>
+        Change Title
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default ErrorExample;
--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+
+// Basic Rules for Hooks
+// =======================================
+// 1. Must start with use
+// 2. component name must be uppercase
+// 3. must be in the function/component body
+// 4. cannot call conditionally
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  // console.log(useState("hello world"));
+
+  // const value = useState(1)[0];
+  // const handler = useState(1)[0];
+  // console.log(value, handler);
+
+  const [text, setText] = useState("random title");
+
+  const handleClick = () => {
+    if (text === "random title") {
+      setText("New Value");
+    } else {
+      setText("random title");
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <h1>{text}</h1>
+      <button className="btn" onClick={handleClick}>
+        Change Title
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateBasics;
